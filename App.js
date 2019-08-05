86f1adfb9427f93d0f2945386df11082078fbf44
hambog00 <hambog00@gmail.com>
2019-08-05 15:10:12 +0800

    Redux Setup

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
+import {AppRegistry} from 'react-native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Index from './pages/index';
+import {Provider} from 'react-redux';
+import AppIndex from './pages/index';
+import {createStore} from 'redux';
+import Reducer from './Reducer';
+
+
+const store = createStore(Reducer);
+
+const AppContainer = () =>
+  <Provider store={store}>
+    <AppIndex/>
+  </Provider>
 
 export default function App() {
-  return (
-    <View>
-      <Index/>
-    </View>
-  );
+  return AppContainer();
 }
+
+//AppRegistry.registerComponent("chatApp", () => AppContainer);
+
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import {isLoggedIn, isLoggedOut} from '../Action';
 
-export default class index extends React.Component{
+class index extends React.Component{
+
+  constructor(props){
+    super(props);
+      this.state = {
+        login_status : false
+      }
+  }
+
     render(){
+      console.log(this.props);
         return (
             <View>
+              <Button onPress={this.props.isLoggedIn} title="TEST LOGIN"/>
               <Text>This is the index yeah!</Text>
             </View>
           );
     }  
 }
+
+function mapStateToProps(state){
+  return{
+    login_status : state.login_status
+  }
+}
+
+export default connect(mapStateToProps, {isLoggedIn})(index);
 
