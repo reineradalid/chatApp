b018a11c6f21cb3bef6838f368276c6b1f5cbc86
reineradalid <reineradalid96@gmail.com>
2019-08-05 15:08:15 +0800

    asd

--- App.js
+++ App.js
@@ -1,19 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView,StatusBar } from 'react-native';
+import{ createDrawerNavigator, createAppContainer, NavigationActions, DrawerItems, } from 'react-navigation'
+import Login from './pages/login.js';
+import About from './pages/drawer.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+export default class App extends React.Component {
+  render() {
+    return (
+        
+    <SafeAreaView style={{flex: 1, backgroundColor: '#fff', marginTop: StatusBar.currentHeight}}>
+      <View>
+      <StatusBar backgroundColor="blue" barStyle="light-content" />
+      <View>
+        <StatusBar hidden={false} />
+      </View>
     </View>
-  );
+    <View style={{flex: 1}}>
+    <MyApp/>
+    </View>
+    </SafeAreaView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const DrawerContent = props =>(
+  <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+  <View style={{flex:1}} >
+    
+      <View>
+    <DrawerItems  style={{fontSize:30}} {...props}/>
+    </View>
+  </View>
+   </SafeAreaView>
+ 
+);
+
+const DrawerNavigator = createDrawerNavigator({
+  Login:{
+    screen:Login,
+    navigationOptions: {
+      drawerLabel: 'Login',
+      // drawerIcon: ({focused, tintColor}) => (
+      //   <Icon name="home"  color={focused?color="#fff": color="#1999CE"} style={{fontSize:25,}}/>
+      // ),
+      }
+  
+    },
+  About:{
+    screen:About,
+    navigationOptions: {
+      drawerLabel: 'Info',
+      // drawerIcon: ({focused, tintColor}) => { return(
+      //   <Icon name="info"  
+      //           color={focused?color="#fff": color="#1999CE"} style={{fontSize:25,}}/>
+      // )
+    
+      }
+    },
+  
   },
-});
+  {
+    contentComponent: DrawerContent,
+    drawerWidth:200,
+    drawerType:'front',
+   contentOptions:{
+      activeTintColor :'#ffffff',
+      inactiveTintColor :'#2196F3',
+      activeBackgroundColor :'#2196F3',
+      inactiveBackgroundColor :'#ffffff',
+      labelStyle:{
+        fontSize:20,
+        fontStyle:"italic"
+      },
+      iconContainerStyle: {
+        opacity: 1,
+      }
+    }
+  },
+)
+const MyApp =createAppContainer(DrawerNavigator);
+
