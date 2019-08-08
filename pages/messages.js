765e9c5b268a165fd0f74e4ca6214765e10baa7b
reineradalid <reineradalid96@gmail.com>
2019-08-08 09:32:29 +0800

    asd

--- pages/messages.js
+++ pages/messages.js
@@ -69,10 +69,10 @@ export  class MessageTrends extends React.Component{
                     <View style={styles.header}>
     
                         <View  style={{ marginTop:20, marginLeft:12, flexDirection:'row', height:60}}>
-                            <TouchableOpacity style={{marginTop:20, marginLeft:5}}>
+                            <TouchableOpacity style={{marginTop:20, marginLeft:5, flex:3}}>
                                 <Icon name="bars" size={30} color="#000" style={{textAlign:'left', flexDirection:'column'}} />   
                             </TouchableOpacity>                     
-                            <View style={{alignItems:'center', justifyContent:'center',}}>      
+                            <View style={{alignItems:'center', justifyContent:'center',flexDirection:'column' ,flex:2, marginRight:10,marginTop:5}}>      
                                 <Text style={{fontSize:25, textAlign:'center', fontWeight:"bold"}}>Messages</Text>
                             </View>
                         </View>
