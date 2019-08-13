8456734d4893c1866d60b6b9c6bfe21dafeaf3f4
Reiner <reineradalid96@gmail.com>
2019-08-13 14:02:16 +0800

    asd

---
+++ pages/subcomponents/createTask.js
@@ -0,0 +1,280 @@
+import React from 'react'
+import {View, 
+        Text , 
+        StyleSheet, 
+        KeyboardAvoidingView, 
+        TextInput,
+        Picker,
+        ScrollView 
+    } from 'react-native';
+import MultiSelect from 'react-native-multiple-select';
+import DatePicker from 'react-native-datepicker'
+
+
+const items = [
+    {
+    id: 'nahs75a5sg',
+    name: 'Lagos',
+  },  {
+    id: 'sdhyaysdj',
+    name: 'Kaduna',
+  }, {
+    id: 'suudydjsjd',
+    name: 'Abuja',
+  }];
+
+export default class CreateTask extends React.Component{
+    constructor(props){
+        super(props)
+        this.onChange = value => {
+            this.setState({ value });
+          };
+        this.state = {
+            startDate:"",
+            endDate:'',
+            priorityLevel:'',
+            selectedItems :[],
+            newTaskDescription:''
+        }
+    }
+
+
+    onSelectedItemsChange = selectedItems => {
+        this.setState({ selectedItems });
+      }
+    render(){
+        const { selectedItems } = this.state;
+        return(
+            <View style={{flex:1}}>
+                <View style={styles.header}>
+                
+                    <View  style={{ marginTop:5, marginLeft:12, flexDirection:'row'}}>
+                                            
+                        <View style={{alignItems:'center', justifyContent:'center',flexDirection:'column' , marginRight:10,marginTop:5, flexGrow:1}}>      
+                            <Text mul style={{fontSize:25, fontWeight:"bold", color:'#fff'}}>Create Task</Text>
+                        </View>
+                    </View>
+                    {/* <TouchableHighlight style={styles.searchBarStyle}>
+                        
+                        <TextInput
+                        placeholder="Search"
+                        style={{ flex:1, width:'100%', marginLeft:10, marginRight:10}}
+                        />
+                    </TouchableHighlight> */}
+                </View>
+                <View style={styles.body}>
+                    <KeyboardAvoidingView>
+                        <View style={{flexDirection:'row'}}>
+                            <Text style={{fontSize:18, fontWeight:'500',marginTop:10, marginLeft:5,flex:1}}>Title: </Text>
+                            <View style={{marginTop:10, marginRight:10,borderColor:'gray', borderWidth:0.5, borderRadius:8,flex:6,}}>
+                                <TextInput placeholder="Input title" style={{ marginLeft:5, color:'#000', fontSize:18, }}/>
+                            </View>
+                            {/* <Picker
+                                placeholder="Select"
+                                selectedValue={this.state.priorityLevel}
+                                style={{height: 55, flex:2, fontSize:18}}
+                                onValueChange={(itemValue, itemIndex) =>
+                                    this.setState({priorityLevel: itemValue})
+                                }>
+                                <Picker.Item label="low" value="low" />
+                                <Picker.Item label="Normal" value="Normal" />
+                                <Picker.Item label="High" value="High" />
+                            </Picker> */}
+                          
+                        </View>
+                        <View style={{flexDirection:'row'}}>
+                            <Text style={{fontSize:18, fontWeight:'500',marginTop:20, marginLeft:5,flex:2}}>Assignee: </Text>
+                                <View style={{marginTop:10, marginRight:10, flex:5,}}>
+                                    <MultiSelect
+                                        hideTags
+                                        items={items}
+                                        uniqueKey="id"
+                                        ref={(component) => { this.multiSelect = component }}
+                                        onSelectedItemsChange={this.onSelectedItemsChange}
+                                        selectedItems={selectedItems}
+                                        selectText="  Select Assignee"
+                                        searchInputPlaceholderText="Select Assignee..."
+                                        onChangeInput={ (text)=> console.log(text)}                          
+                                        tagRemoveIconColor="#CCC"
+                                        tagBorderColor="#CCC"
+                                        tagTextColor="#CCC"
+                                        selectedItemTextColor="#CCC"
+                                        selectedItemIconColor="#CCC"
+                                        itemTextColor="#000"
+                                        displayKey="name"
+                                        searchInputStyle={{ color: '#CCC' }}
+                                        submitButtonColor="#1A3C6B"
+                                        submitButtonText="Submit"
+                                    />
+                                </View>
+                        </View>
+                        <View style={{height:300, marginTop:10, borderTopColor:'#F26725', borderTopWidth:0.5, marginLeft:5, marginRight:5}}>
+                                <Text style={{ textAlign: "left", fontSize:18, fontWeight:'500', marginTop:10 }}>Description:</Text>
+                                <ScrollView style={{maxHeight:250,backgroundColor:'rgba(220, 220, 220, 0.3)', borderRadius:8, marginTop:20 }}>
+                                    <TextInput   
+                                                
+                                                multiline={true} 
+                                                editable = {true}  
+                                                // value={this.state.newTaskDescription} 
+                                                onChangeText={(value) => this.setState({ value })}
+                                                style={{ maxHeight:'100%', 
+                                                flexDirection:'row',
+                                                flexGrow:1,
+                                                marginLeft:5,
+                                                padding:10,
+                                                marginTop:10, 
+                                                alignItems:this.multiline=true?"flex-start":"center",
+                                                textAlignVertical: 'top',
+                                                // lineHeight: 23,
+                                                flex: 2,
+                                                fontSize:18,
+                                            }} />
+                                </ScrollView>
+                            </View>
+                            <View style={{ marginTop:10, marginBottom:10,  borderBottomColor:'#F26725', borderBottomWidth:0.5, marginLeft:5, marginRight:5, paddingBottom:10}}>
+                                <Text style={{ textAlign: "left", fontSize:18, fontWeight:'500', marginTop:10 }}>Select Date:</Text>
+                            </View>
+                            <View style={{flexDirection:'row', marginTop:10}}>
+                                <DatePicker
+                                    style={{flex:3, marginLeft:10, marginRight:10}}
+                                    date={this.state.startDate}
+                                    mode="date"
+                                    placeholder="start date"
+                                    format="YYYY-MM-DD"
+                                    minDate="2016-05-01"
+                                    maxDate="2022-01-01"
+                                    confirmBtnText="Confirm"
+                                    cancelBtnText="Cancel"
+                                    customStyles={{
+                                    dateIcon: {
+                                        position: 'absolute',
+                                        left: 0,
+                                        top: 4,
+                                        marginLeft: 0
+                                    },
+                                    dateInput: {
+                                        marginLeft: 36
+                                    }
+                                    // ... You can check the source to find the other keys.
+                                    }}
+                                    onDateChange={(date) => {this.setState({startDate: date}), console.log(this.state.startDate)}}
+                                />
+                                <DatePicker
+                                        style={{ flex:3, marginLeft:10, marginRight:10}}
+                                        date={this.state.endDate}
+                                        mode="date"
+                                        placeholder="end date"
+                                        format="YYYY-MM-DD"
+                                        minDate="2016-05-01"
+                                        maxDate="2022-01-01"
+                                        confirmBtnText="Confirm"
+                                        cancelBtnText="Cancel"
+                                        customStyles={{
+                                        dateIcon: {
+                                            position: 'absolute',
+                                            left: 0,
+                                            top: 4,
+                                            marginLeft: 0
+                                        },
+                                        dateInput: {
+                                            marginLeft: 36
+                                        }
+                                        // ... You can check the source to find the other keys.
+                                        }}
+                                        onDateChange={(date) => {this.setState({endDate: date}), console.log(this.state.endDate)}}
+                                    />
+                            </View>
+                            <View style={{ marginTop:10, borderBottomColor:'#F26725', borderBottomWidth:0.5, marginLeft:5, marginRight:5, paddingBottom:10, flexDirection:'column'}}>
+                                <Text style={{ textAlign: "left", fontSize:18, fontWeight:'500', marginTop:10 }}>Priority Level:</Text>
+                                
+                                   
+                                
+                            </View>
+                            <Picker
+                                        placeholder="Select"
+                                        selectedValue={this.state.priorityLevel}
+                                        style={{height: 55, fontSize:18, margin:10, borderWidth:0.5, borderBottomColor:'gray', borderRadius:8}}
+                                        onValueChange={(itemValue, itemIndex) =>
+                                            this.setState({priorityLevel: itemValue})
+                                        }>
+                                        <Picker.Item label="low" value="low" />
+                                        <Picker.Item label="Normal" value="Normal" />
+                                        <Picker.Item label="High" value="High" />
+                                    </Picker>
+                                
+                           
+
+                    </KeyboardAvoidingView>
+                </View>
+            </View>
+        )
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+    nameStyle:{
+        marginLeft:20, 
+        fontSize:20, 
+        fontWeight:'bold',
+        color:'#fff', 
+        marginTop:5,
+
+    },sampleMessage:{
+        marginLeft:20, 
+        fontSize:18, 
+        color:'#fff', 
+        marginRight:10, 
+        textAlign: 'justify'
+    },
+    imageStyle:{
+        width: 60, 
+        height: 60, 
+        borderRadius:50,
+    },
+    searchBarStyle:{
+        flexDirection:"row",
+        height: 50, 
+        borderColor:'black',
+        backgroundColor:'rgba(220,220,220, 0.5)',
+        marginRight:10, 
+        marginLeft: 10,  
+        alignItems:"center",
+    },
+    header:{
+        height: 70, 
+        marginBottom:5,
+        backgroundColor: '#F26725', 
+        justifyContent: "center", 
+        alignContent: "center",
+       
+    },
+    body:{  
+        marginLeft:10,
+        marginRight:10,
+        marginTop:20
+      
+    },
+    message:{
+        height:70,
+        marginLeft:5,
+        marginRight:5,
+        marginBottom:10,
+        marginTop:10,
+        alignItems:'center', 
+        flexDirection:'row-reverse'
+    } ,
+    buttonStyle:{
+        height: 50, 
+        backgroundColor: '#009fff', 
+        marginBottom:10,
+        borderRadius: 8,
+        width: 100,
+        justifyContent:"center",
+        alignItems:"center",
+        marginLeft:140
+
+    }
+   });
+
--- pages/task.js
+++ pages/task.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Text, View, StyleSheet,StatusBar} from 'react-native';
+import {Text, View, StyleSheet,StatusBar, TextInput} from 'react-native';
 import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { createStackNavigator, createAppContainer} from 'react-navigation';
@@ -13,6 +13,7 @@ import {
     Toast,
     Provider,
   } from '@ant-design/react-native';
+  import CreateTask from './subcomponents/createTask'
 
 
 export default class Task extends Component {
@@ -51,82 +52,98 @@ export default class Task extends Component {
         this.state = {
             visible: false,
             title:'SAMPLE',
+            taskmodalPriority:'High',
+            modalStartDate:'02/09/2019',
+            modalEndDate:'02/09/2019',
+            modalPrioColor:'#FF0000',
+            taskDescription:'People often install a kitty door, only to discover that they have a problem. The problem is their cat will not use the kitty door. There are several common reasons why cats won’t use kitty doors. First, they may not understand how a kitty door works. They may not understand that it is a little doorway just for them. Second, many kitty doors are dark, and cats cannot see to the other side. As such, they can’t be sure of what is on the other side of the door, so they won’t take the risk. One last reason cats won’t use kitty doors is because some cats don’t like the feeling of pushing through the door and having the door drag across their back. But don’t worry—there is a solution for this kitty-door problem. ',
             tasks:[
                 {
                     id:'1',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'2',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Normal',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'3',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Normal',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'4',
                     Task:'Sample task',
                     Desc:'sapmple description', priorityLevel:'High',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'5',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'6',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'7',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'8',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'9',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'10',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 },
                 {
                     id:'11',
                     Task:'Sample task',
                     Desc:'sapmple description',
                     priorityLevel:'Low',
-                    date:'2019-15-03'
+                    startDate:'02/09/2019',
+                    endDate:'02/09/2019'
                 }
             ]
         }
@@ -175,7 +192,8 @@ export default class Task extends Component {
                                                 <View style={{flexDirection:'column', flex:5}}>
                                                         <Text style={styles.nameStyle}>{taskList.Task}</Text>
                                                         <Text style={styles.sampleMessage}>{taskList.Desc}</Text>
-                                                        <Text style={{ fontSize:12, color:"#fff",marginLeft:20}}>{taskList.date}</Text>
+                                                        <Text style={{ fontSize:12, color:"#fff",marginLeft:20}}>{taskList.startDate} - {taskList.endDate}</Text>
+                                                        
                                                  </View>
                                                
                                                     <View style={{ flex:1,backgroundColor:'#fff', flexDirection:'column-reverse',borderRadius:50, height:'30%', margin:5, justifyContent:'center', alignItems:'center'}}>
@@ -206,24 +224,52 @@ export default class Task extends Component {
                             elevation={1000}
                             offsetY={10}
                             buttonColor="rgba(231,76,60,1)"
-                            onPress={() => {this.setState({ visible: true })}}
+                            onPress={() => {this.props.navigation.navigate('CreateTask')}}
                             />
 
                     <Modal
-                        title={this.state.title}
+                        title={<Text style={{fontWeight:'bold', fontSize:20,}}>{this.state.title}</Text>}
                         transparent
                         onClose={this.onClose}
                         maskClosable
                         visible={this.state.visible}
                         // closable
                         footer={footerButtons}
-                        style={{width:"98%", height:"70%", marginLeft:10, marginRight:10}}
+                        style={{width:"98%", marginLeft:15, marginRight:15}}
                         >
-                        <View style={{ paddingVertical: 20 }}>
-                            <Text style={{ textAlign: 'center' }}>Content...</Text>
-                            <Text style={{ textAlign: 'center' }}>Content...</Text>
-                        </View>
-                        
+                       
+                        <View style={{height:300, marginTop:10, borderTopColor:'#F26725', borderTopWidth:0.5, marginLeft:5, marginRight:5}}>
+                            <Text style={{ textAlign: "left", fontSize:18, fontWeight:'500', marginTop:10 }}>Description:</Text>
+                                <ScrollView style={{maxHeight:250,backgroundColor:'rgba(220, 220, 220, 0.3)', borderRadius:8, marginTop:20 }}>
+                                    <TextInput   
+                                                
+                                                multiline={true} 
+                                                editable = {false}  
+                                                value={this.state.taskDescription} 
+                                                style={{ maxHeight:'100%', 
+                                                flexDirection:'row',
+                                                flexGrow:1,
+                                                marginLeft:5,
+                                                padding:10,
+                                                marginTop:10, 
+                                                alignItems:this.multiline=true?"flex-start":"center",
+                                                textAlignVertical: 'top',
+                                                // lineHeight: 23,
+                                                flex: 2,
+                                                fontSize:18,
+                                            }} />
+                                </ScrollView>
+                    
+                           
+                            </View>
+                            <View style={{flexDirection:'column-reverse'}}>
+                                <View style={{flexDirection:'row',}}>                                
+                                    <Text style={{ textAlign: 'left', flex:5, margin:10, fontSize:15}}>{this.state.modalStartDate} - {this.state.modalEndDate}</Text>
+                                    <Text style={{ textAlign: 'center', flex:1 ,fontSize:15, fontWeight:'500',margin:10,backgroundColor:this.state.modalPrioColor, borderRadius:8}}>{this.state.taskmodalPriority}</Text>  
+                                </View>
+                            </View>
+                           
+                       
                     </Modal>
 
                     
@@ -250,6 +296,14 @@ export default class Task extends Component {
          }),
          
        },
+       CreateTask: {
+        screen: CreateTask,
+        navigationOptions: () => ({
+          title: `MessageTrends`,
+          headerBackTitle: null
+        }),
+        
+      },
         
      },
      {
